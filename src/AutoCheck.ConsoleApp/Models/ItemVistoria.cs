819ce50a7a435737b1cf045bcb35695e32d65bc4
sara <sara denardin>
2026-08-24 14:34:47 -0300

    Implementa o construtor da classe ItemVistoria e o sistema de validação para receber apenas os valore 'bom', 'regular' ou 'ruim'

--- src/AutoCheck.ConsoleApp/Models/ItemVistoria.cs
+++ src/AutoCheck.ConsoleApp/Models/ItemVistoria.cs
@@ -7,7 +7,15 @@ namespace AutoCheck.ConsoleApp.Models
 
         public ItemVistoria(string nome, string status)
         {
-            
+            this.Nome = nome;
+            if (status == "bom" || status == "regular" || status == "ruim")
+            {
+                this.Status = status;
+            }
+            else
+            {
+                Console.WriteLine("Status inválido. Por favor inserir apenas 'bom', 'ruim' ou 'regular'");
+            }
         }
     }
 }
