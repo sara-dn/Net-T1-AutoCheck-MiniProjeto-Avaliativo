2ce72c5a5af4bbc5115bffb1a2a8936404c9535e
sara <sara denardin>
2026-08-24 17:14:54 -0300

    Implementa atributos e construtores a classe MotorVistoria, construtores calculam percentual de aprovação, geram status de aprovação e ação corporativa recomendada

--- src/AutoCheck.ConsoleApp/Services/MotorVistoria.cs
+++ src/AutoCheck.ConsoleApp/Services/MotorVistoria.cs
@@ -1,7 +1,44 @@
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+
 namespace AutoCheck.ConsoleApp.Services
 {
     public class MotorVistoria
     {
+        public int NotaMaxima {get;set;}
+        public int NotaObtida {get; set;}
+        public double Percentual {get; private set;}
+        public string StatusAprovacao {get; private set;}
+        public string AcaoCorporativa {get; private set;}
+        public MotorVistoria(int totaldeitems, int notaobitida)
+        {
+            this.NotaMaxima = totaldeitems*10;
+            this.NotaObtida = notaobitida;
+
+            //calculo do percentaul aprovação
+            this.Percentual = (NotaObtida/NotaMaxima)*100;        
+        }
+        
+        public MotorVistoria()
+        {
+            if(Percentual >= 90)
+            {
+                this.StatusAprovacao = "Aprovado com Excelência";
+                this.AcaoCorporativa = "Liberado para compra/revenda imediata.";
+            }
+            if(Percentual >= 60 && Percentual <= 89)
+            {
+                this.StatusAprovacao = "Aprovado com Apontamentos";
+                this.AcaoCorporativa = "Exige desconto na compra para reparos da oficina.";
+            }
+            else
+            {
+                this.StatusAprovacao = "Reprovado na Vistoria";
+                this.AcaoCorporativa = "Veículo recusado pela concessionária.";
+            }
+        }
         
     }
 }
